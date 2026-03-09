267a9b29efb602a280d233e2c7969598bd36edae
isai245 <pollito.piopio707@gmail.com>
2026-03-08 22:46:39 -0500

    INTENTO DE BASE DE DATOS
    FALLO

---
+++ js/DAATABASE.js
@@ -0,0 +1,23 @@
+/*<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCHVZjzvUonIyUS9C43NnXUubsx1AVW05A",
+    authDomain: "inventario-del-rio.firebaseapp.com",
+    projectId: "inventario-del-rio",
+    storageBucket: "inventario-del-rio.firebasestorage.app",
+    messagingSenderId: "31921330346",
+    appId: "1:31921330346:web:f710fb7e2bb2f8156118d3",
+    measurementId: "G-9ELKE52BNK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>*/
